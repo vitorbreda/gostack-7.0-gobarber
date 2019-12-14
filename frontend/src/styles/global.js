2a87247d5b570d15520caac921ba39f99f2dca0c
Vitor Breda <vitor@olivasdigital.com.br>
2019-12-14 00:27:26 -0300

    Removing submodule

---
+++ frontend/src/styles/global.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  *{
+    margin:0;
+    padding:0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  *:focus{
+    outline:0;
+  }
+  html, body, #root{
+    height:100%;
+    background: linear-gradient(-90deg, #7159c1, #ab59c1);
+  }
+  body{
+    -webkit-font-smoothing: antialiased;
+  }
+  body, input, button{
+    font: 14px 'Roboto', sans-serif;
+  }
+  a{
+    text-decoration: none;
+  }
+  li{
+    list-style: none;
+  }
+  button{
+     cursor: pointer;
+  }
+`;
